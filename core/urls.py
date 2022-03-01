0c90a3b9e500c02c44c14751040f76fd79748926
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2022-03-01 04:03:07 -0300

    projects pages

--- core/urls.py
+++ core/urls.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from .views import *
 
-urlpatterns = [path("", index, name="index")]
+urlpatterns = [
+    path("", index, name="index"),
+    path("project", project, name="project"),
+    path("projects/<int:id>", project_id, name="project_id"),
+]
--- core/views.py
+++ core/views.py
@@ -2,5 +2,15 @@ from django.shortcuts import render
 
 
 def index(request):
-    template_name = "index.html"
+    template_name = "projects/index.html"
+    return render(request, template_name)
+
+
+def project(request):
+    template_name = "projects/project.html"
+    return render(request, template_name)
+
+
+def project_id(request, id):
+    template_name = "projects/project_id.html"
     return render(request, template_name)
