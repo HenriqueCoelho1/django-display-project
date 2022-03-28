c6a43466d0546b8e4dd787b2c5b1acdfc5f5aad0
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2022-03-28 02:33:53 -0300

    CRUD

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,9 @@ from .views import *
 
 urlpatterns = [
     path("", index, name="index"),
-    path("projects", projects, name="projects"),
-    path("project/<str:id>", single_project, name="single_project"),
+    path("projects/", projects, name="projects"),
+    path("project/<str:id>/", single_project, name="single_project"),
+    path("create-project/", create_project, name="create_project"),
+    path("update-project/<str:id>/", update_project, name="update_project"),
+    path("delete-project/<str:id>/", delete_project, name="delete_project"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
 from core.models import Project
+from .forms import ProjectForm
 
 
 def index(request):
@@ -9,19 +9,56 @@ def index(request):
 
 
 def projects(request):
-    context = {}
+    content = {}
     template_name = "projects/projects.html"
     projects = Project.objects.all()
-    context.update({"projects": projects})
-    return render(request, template_name, context)
+    content.update({"projects": projects})
+    return render(request, template_name, content)
 
 
 def single_project(request, id):
-    context = {}
+    content = {}
     template_name = "projects/single_project.html"
     project = Project.objects.get(id=id)
-    context.update({"project": project})
+    content.update({"project": project})
     tags = project.tags.all()
-    context.update({"tags": tags})
+    content.update({"tags": tags})
 
     return render(request, template_name, {"project": project})
+
+
+def create_project(request):
+    template_name = "projects/project_form.html"
+    form = ProjectForm()
+
+    if request.method == "POST":
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("projects")
+    content = {"form": form}
+    return render(request, template_name, content)
+
+
+def update_project(request, id):
+    template_name = "projects/project_form.html"
+    project = Project.objects.get(id=id)
+    form = ProjectForm(instance=project)
+
+    if request.method == "POST":
+        form = ProjectForm(request.POST, instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect("projects")
+    content = {"form": form}
+    return render(request, template_name, content)
+
+
+def delete_project(request, id):
+    template_name = "projects/delete_template.html"
+    project = Project.objects.get(id=id)
+    if request.method == "POST":
+        project.delete()
+        return redirect("projects")
+    context = {"object": project}
+    return render(request, template_name, context)
